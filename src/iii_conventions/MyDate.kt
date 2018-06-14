2c2122ad23ebe5d4b355e3f9b8a3fbec7c01408a
Ziiangulov Eduard z003xzbz <eduard.ziiangulov@siemens.com>
2018-06-14 15:20:42 +0300

    more tests solved

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,16 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        val compareYears = year.compareTo(other.year)
+        val compareMonths = month.compareTo(other.month)
+        val compareDays = dayOfMonth.compareTo(other.dayOfMonth)
+
+        return if (compareYears == 0) if (compareMonths == 0) compareDays else compareMonths else
+            compareYears
+    }
+
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
